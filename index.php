a614797dab8f6ca54de4419c21b051a15eb7e437
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-16 16:58:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 				<div class="col-md-2 col-lg-2 col-xs-12 col-sm-12"></div>
 				<div class="col-md-10 col-lg-10 col-xs-12 col-sm-12">
 				<?php
+					$description0=["KTM Duke","CC : 250 ","Mileage : 35kmpl","Engine : dual Exhaust","Cost : $2000"];
 					$description1=["HP Pavilion","Harddisk : 20 TB ","RAM : 124GB","Screen Resolution : 8k","Cost : $1500"];
 					$description2=["Mac Book Pro 2.0","Harddisk : 15 TB ","RAM : 116GB","Screen Resolution : 4k","Cost : $1000"];
 					$description3=["Dell Inspiron","Harddisk : 10 TB ","RAM : 64GB","Screen Resolution : 4k","Cost : $800"];
@@ -21,9 +22,8 @@
 					$description6=["Nokia 6","Harddisk : 64 GB ","RAM : 8GB","Screen Resolution : 8k","Cost : $900"];
 					$description7=["ACER","Harddisk : 200 TB ","RAM : 1024GB","Screen Resolution : 8k","Cost : $10000"];
 					$description8=["ASUS","Harddisk : 200 TB ","RAM : 1024GB","Screen Resolution : 8k","Cost : $10000"];
-					$description9=["KTM Duke","CC : 250 ","Mileage : 35kmpl","Engine : dual Exhaust","Cost : $2000"];
-					$items=[$description1,$description2,$description3,$description4,$description5,$description6,$description7,$description8,$description9];
-					for($i = 0; $i <= 9; $i++)
+					$items=[$description0,$description1,$description2,$description3,$description4,$description5,$description6,$description7,$description8];
+					for($i = 0; $i < 9; $i++)
 					{
 						?>
 							<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" style="margin-top:50px; width: 270px; height 270px;">
