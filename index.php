9ee7ffe7731071ddf01c3f243d5d02b4a244f21f
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-16 15:38:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Shopping cart</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
+        <div class="container">
+		<div class="row">
+			<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+				<div class="col-md-2 col-lg-2 col-xs-12 col-sm-12"></div>
+				<div class="col-md-10 col-lg-10 col-xs-12 col-sm-12">
+                    <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+                    
+                    </div>
+                    <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12"></div>
+                    <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12"></div>
+                </div>
+			</div>
+		</div>
+	</div>
         <?php
         // put your code here
         echo "hello world !!";
