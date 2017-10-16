2873b51af5964b973310d74bb10bc73d721e934e
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-16 16:02:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,9 +12,17 @@
 			<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
 				<div class="col-md-2 col-lg-2 col-xs-12 col-sm-12"></div>
 				<div class="col-md-10 col-lg-10 col-xs-12 col-sm-12">
-                    <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
-                    <img src="lenovo1.png" />
-                    </div>
+				<?php
+					for(i=1;i<=9;i++)
+					{
+						?>
+							<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+							    <img src="<?php echo $i; ?>.png" />
+							</div>
+						<?php
+					}
+				?>
+                    
                     <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12"></div>
                     <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12"></div>
                 </div>
