12fcc6496bb16fb082c1d913d148b99c1a361139
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-16 17:25:53 +0530

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -1,12 +1,19 @@
+<?php
+    $prod_id=(isset($_REQUEST['prod_id']) ? $_REQUEST['prod_id'] : '');
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        echo "hello world !!";
-        ?>
+        <div class="container">
+		     <div class="row">
+			    <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+                    <img src="<?php echo $prod_id; ?>.jpg" class="img-responsive">
+                 </div>
+				<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12"></div>
+             </div>
+         </div>
     </body>
 </html>
