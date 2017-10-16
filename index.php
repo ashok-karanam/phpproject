c321db4a9a800fb9d506a5deff639dc716a0b175
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-16 16:12:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+ <html>
     <head>
         <meta charset="UTF-8">
         <title>Shopping cart</title>
@@ -16,7 +16,7 @@
 					for($i = 1; $i <= 9; $i++)
 					{
 						?>
-							<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" style="margin-top:50px;">
+							<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12" style="margin-top:50px; width: 270px; height 270px;">
 							    <img src="<?php echo $i; ?>.jpg" class="img-responsive">
 							</div>
 						<?php
