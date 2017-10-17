4b27494c9bc59aa13db5c922b94186f8f911fbad
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-17 10:17:21 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,7 +2,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:e-commdb.database.windows.net,1433; Database = shopping", "ashok", "{ironman@13}");
+    $conn = new PDO("sqlsrv:server = tcp:e-commdb.database.windows.net,1433; Database = shopping", "ashok", "ironman@13");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,7 +11,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ashok@e-commdb", "pwd" => "{ironman@13}", "Database" => "shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ashok@e-commdb", "pwd" => "ironman@13", "Database" => "shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:e-commdb.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
