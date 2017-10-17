a84477c10a1cb033541d59dd8f2e45061550e096
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-17 09:56:43 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("http://e-commdb.database.windows.net","ashok","ironman@13","shopping");
+$connection = mysqli_connect("e-commdb.database.windows.net","ashok","ironman@13","shopping");
 if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to Database: " . mysqli_connect_error();
