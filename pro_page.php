b1c9982987688412dc5413d5203d2e3f5c346cb8
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 12:00:42 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -1,9 +1,27 @@
 <?php
     	$prod_id=(isset($_REQUEST['prod_id']) ? $_REQUEST['prod_id'] : '');
 	require_once('db_config.php'); 
-	$sql = "SELECT pro_name FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=prod_id";
+	$sql = "SELECT pro_name FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=$prod_id";
 	foreach($conn->query($sql) as $row)
 	{
     		echo "<li>{$row['pro_name']}</li>";
 	}
 ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+		  <div class="row">
+		      <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+                           <img src=".jpg" class="img-responsive">
+                      </div>
+	              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+		      </div>
+                  </div>
+         </div>
+    </body>
+</html>
