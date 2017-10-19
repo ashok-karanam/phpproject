caeb0d71bafeffaf138f13cd2e22cffa0b8e80b2
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 12:02:07 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -1,7 +1,7 @@
 <?php
     	$prod_id=(isset($_REQUEST['prod_id']) ? $_REQUEST['prod_id'] : '');
 	require_once('db_config.php'); 
-	$sql = "SELECT pro_name FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=$prod_id";
+	$sql = "SELECT * FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=$prod_id";
 	foreach($conn->query($sql) as $row)
 	{
     		echo "<li>{$row['pro_name']}</li>";
