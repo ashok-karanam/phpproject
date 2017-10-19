4ec83e647a4dc1089d1a83f144362cb8a9d16f52
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 12:01:42 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -17,9 +17,16 @@
         <div class="container">
 		  <div class="row">
 		      <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                           <img src=".jpg" class="img-responsive">
+                           <img src="<?php echo $row['img_name']; ?>" class="img-responsive">
                       </div>
 	              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+			      <ul>
+					<li>Name: <?php echo $row['pro_name']; ?></li>
+					<li>Storage: <?php echo $row['storage']; ?></li>
+					<li>Memory: <?php echo $row['memory']; ?></li>
+					<li>Resolution: <?php echo $row['screen_resolution']; ?></li>
+					<li>Price: <?php echo $row['price']; ?></li>
+				</ul>
 		      </div>
                   </div>
          </div>
