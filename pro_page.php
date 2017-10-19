d1716e0c1084cc24ca886134e8cd100e2991198a
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 12:07:11 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div class="container">
-		  <div class="row">
+		  <div class="row" style="padding: 50px;">
 			<?php
 			  	foreach($conn->query($sql) as $row)
 				{
