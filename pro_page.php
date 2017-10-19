7abe99afeac0441942d6ae26a856d4635e8e111e
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 11:56:01 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -16,7 +16,7 @@
         <div class="container">
 		  <div class="row">
 		      <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                           <img src="<?php echo $prod_id; ?>.jpg" class="img-responsive">
+                           <img src=".jpg" class="img-responsive">
                       </div>
 	              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
 		      </div>
