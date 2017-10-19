5a579ddeed6fdd36c4134ec70286e7ec6f76d70d
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-19 12:04:04 +0530

    Update pro_page.php

--- pro_page.php
+++ pro_page.php
@@ -2,8 +2,7 @@
     	$prod_id=(isset($_REQUEST['prod_id']) ? $_REQUEST['prod_id'] : '');
 	require_once('db_config.php'); 
 	$sql = "SELECT * FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=$prod_id";
-	foreach($conn->query($sql) as $row)
-	{
+	
     		echo "<li>{$row['pro_name']}</li>";
 	}
 ?>
@@ -16,18 +15,25 @@
     <body>
         <div class="container">
 		  <div class="row">
-		      <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                           <img src="<?php echo $row['img_name']; ?>" class="img-responsive">
-                      </div>
-	              <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-			      <ul>
-					<li>Name: <?php echo $row['pro_name']; ?></li>
-					<li>Storage: <?php echo $row['storage']; ?></li>
-					<li>Memory: <?php echo $row['memory']; ?></li>
-					<li>Resolution: <?php echo $row['screen_resolution']; ?></li>
-					<li>Price: <?php echo $row['price']; ?></li>
-				</ul>
-		      </div>
+			<?php
+			  	foreach($conn->query($sql) as $row)
+				{
+					?>
+							<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+							   <img src="<?php echo $row['img_name']; ?>" class="img-responsive">
+						      </div>
+						      <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
+							      <ul>
+									<li>Name: <?php echo $row['pro_name']; ?></li>
+									<li>Storage: <?php echo $row['storage']; ?></li>
+									<li>Memory: <?php echo $row['memory']; ?></li>
+									<li>Resolution: <?php echo $row['screen_resolution']; ?></li>
+									<li>Price: <?php echo $row['price']; ?></li>
+								</ul>
+						      </div>
+			  		<?php
+				}
+			?>
                   </div>
          </div>
     </body>
