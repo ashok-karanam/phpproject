2dfab899c406929776879639c0553c79d71b0b26
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-20 16:22:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	{
 		$append="where (cat_name='Mobile'";
 	}
-	$sql = "SELECT * FROM tbl_prod inner join tbl_catg on tbl_prod.cat_id=tbl_catg.cat_id";
+	$sql = "SELECT * FROM tbl_prod inner join tbl_catg on tbl_prod.cat_id=tbl_catg.cat_id where tbl_catg.cat_name='Mobiles'";
 ?>
 
 <html>
