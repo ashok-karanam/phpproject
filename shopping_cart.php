2de8a60351035e2986e0f2ef5def9172e8e778a1
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-20 11:29:59 +0530

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -1,11 +1,65 @@
 <?php
 	session_start();
-if(empty($_SESSION['cart_arr']))
-{
-	echo "empty";
-}
-else
-{
-	echo "t";
-} 
+	require_once('db_config.php'); 
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Shopping Cart</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>
+	    <div class="container">
+		  <div class="row" style="padding: 50px;">
+			<?php
+				foreach($_SESSION['cart_arr'] as $values)
+				{
+					$sql = "SELECT * FROM tbl_prod inner join tbl_catg on tbl_catg.cat_id=tbl_prod.cat_id where pro_id=$value";
+
+					foreach($conn->query($sql) as $row)
+					{
+						?>
+							<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+								<div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+									<img src="<?php echo $row['img_name']; ?>" class="img-responsive">
+								</div>
+								<div class="col-md-5 col-lg-5 col-xs-12 col-sm-12">
+									<ul>
+										<li>Name: <?php echo $row['pro_name']; ?></li>
+										<li>Product Id: <?php echo $row['pro_id']; ?></li>
+										<li>Price: <?php echo $row['price']; ?></li>
+									</ul>
+								 </div>
+								<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+									<form action="" method="post">
+										<input type="hidden" value="<?php echo $row['pro_id'];?>" name="pro_id">
+										<button type="delete" value="Delete" name="delete_now">Delete</button>
+									</form>      
+								</div>
+							</div>	
+						<?php
+					}
+				}
+			?>
+                  </div>
+         </div>
+    </body>
+</html> 
+
+<?php
+	if(isset($_POST['delete_now']))
+	{
+		unset($_SESSION['pro_id']);
+		unset($prod_id);
+		?>
+			<script>
+				alert("Product Deleted");
+				window.location.href=window.location.href;
+			</script>
+		<?php
+		
+	}
 ?>
