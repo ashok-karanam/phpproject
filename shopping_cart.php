66f138730fac1b10a13ae1bdfa7db6116e54321d
ashok-karanam <ashok.karanam1996@gmail.com>
2017-10-20 10:51:20 +0530

    Update shopping_cart.php

--- shopping_cart.php
+++ shopping_cart.php
@@ -6,38 +6,3 @@
 		echo $value;
 	} 
 ?>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Shopping Cart</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-    
-    <body>
-	    <div class="container">
-		  <div class="row" style="padding: 50px;">
-			<?php
-			  		
-			?>
-                  </div>
-         </div>
-    </body>
-</html> 
-
-<?php
-	if(isset($_POST['delete_now']))
-	{
-		unset($_SESSION['pro_id']);
-		unset($prod_id);
-		?>
-			<script>
-				alert("Product Deleted");
-				window.location.href=window.location.href;
-			</script>
-		<?php
-		
-	}
-?>
